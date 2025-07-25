b439eff60c63a8312b0c6afe1317f33cd2194bbd
Bevilhonda <marcelinyuri@gmail.com>
2025-07-25 09:36:55 -0300

    atualiza api

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -1,8 +1,6 @@
-
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Bevilhonda/Portifolio-desafio-DIO/main/data/profile.json'
-
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+  const url = 'https://raw.githubusercontent.com/Bevilhonda/Portifolio-desafio-DIO/main/data/profile.json';
+  const response = await fetch(url);
+  const profileData = await response.json();
+  return profileData;
 }
